5adc9a51e90f2a9000f7020cbd9c386a5c5049fb
ftne <andrei.kubrakov@gmail.com>
2022-08-01 16:08:01 +0300

    add service protobuf, add grpc dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("com.google.protobuf") version "0.8.19"
 }
 
 group = "org.example"
@@ -12,7 +13,21 @@ repositories {
 }
 
 dependencies {
+    implementation("io.grpc:grpc-netty:" + Version.GRPC)
+    implementation("io.grpc:grpc-protobuf:" + Version.GRPC)
+    implementation("io.grpc:grpc-stub:" + Version.GRPC)
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+    implementation("com.google.protobuf:protobuf-kotlin:" + Version.PROTOBUF)
+    implementation("com.google.protobuf:protobuf-java:" + Version.PROTOBUF)
     testImplementation(kotlin("test"))
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/resources/proto")
+        }
+    }
 }
 
 tasks.test {
@@ -21,4 +36,9 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+companion object Version {
+    const val GRPC = "1.47.0"
+    const val PROTOBUF = "3.21.1"
 }
